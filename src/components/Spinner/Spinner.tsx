20f9866921bec12f982ee7e83b1144cddd717f64
Phu N <phund@snapinnovations.vn>
2021-05-11 15:40:49 +0700

    change loading icon to daruma

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,52 +1,29 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import PanIcon from "./PanIcon";
-import PancakeIcon from "./PancakeIcon";
+import DarumaIcon from "./DarumaIcon";
 import { SpinnerProps } from "./types";
-
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const float = keyframes`
-	0% {
-		transform: translatey(0px);
-	}
-	50% {
-		transform: translatey(10px);
-	}
-	100% {
-		transform: translatey(0px);
-	}
-`;
 
 const Container = styled.div`
   position: relative;
 `;
 
-const RotatingPancakeIcon = styled(PancakeIcon)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  animation: ${rotate} 2s linear infinite;
-  transform: translate3d(0, 0, 0);
+const scale = keyframes`
+	0% {
+		transform: scale(1);
+	}
+	100% {
+		transform: scale(1.2);
+	}
 `;
 
-const FloatingPanIcon = styled(PanIcon)`
-  animation: ${float} 6s ease-in-out infinite;
-  transform: translate3d(0, 0, 0);
+const ScaleDarumaIcon = styled(DarumaIcon)`
+  animation: ${scale} 1s ease-in-out infinite;
 `;
 
-const Spinner: React.FC<SpinnerProps> = ({ size = 128 }) => {
+const Spinner: React.FC<SpinnerProps> = ({ size = 51 }) => {
   return (
     <Container>
-      <RotatingPancakeIcon width={`${size * 0.5}px`} />
-      <FloatingPanIcon width={`${size}px`} />
+      <ScaleDarumaIcon width={`${size}px`} />
     </Container>
   );
 };
